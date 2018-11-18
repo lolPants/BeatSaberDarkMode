45f81b955c9c3feff1d19ec7d8df5f940fc042f7
Taft <sdyessdev@gmail.com>
2018-11-17 19:29:52 -0600

    Fix diff colors on global charts

---
+++ extension/fixStupidInlineColors.js
@@ -0,0 +1,7 @@
+const posScores = document.querySelectorAll('span[style="color:#F94022;"]');
+const negScores = document.querySelectorAll('span[style="color:#42B129;"]');
+const neuScores = document.querySelectorAll('span[style="color:#363636;"]');
+
+posScores.forEach((score) => {score.style="color:#00ff00;"});
+negScores.forEach((score) => {score.style="color:#ff0000;"});
+neuScores.forEach((score) => {score.style="color:#eee;"});
